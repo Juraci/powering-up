5ec6d09c03d54ddb056305419085cb96be4287da
Juraci Vieira <juraci.vieira@gmail.com>
2017-06-20 12:04:52 -0300

    listing topics

--- src/StoryBox/index.jsx
+++ src/StoryBox/index.jsx
@@ -3,9 +3,21 @@ import React, { Component } from 'react';
 class StoryBox extends Component {
 
   render() {
-    return(<div>Story Box</div>);
-  }
+    const now = new Date();
+    const topicList = ['HTML', 'Javascript', 'React'];
 
+    return(
+      <div>
+        <h3>Stories App</h3>
+        <p className="lead">
+          Current Time: { now.toTimeString() }
+        </p>
+        <ul>
+          { topicList.map(topic => <li>{topic}</li>) }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default StoryBox;
