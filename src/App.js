dc898e07e94685fe1a089a64774ff56bb1a0938f
Juraci Vieira <juraci.vieira@gmail.com>
2017-06-20 11:43:59 -0300

    first component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import StoryBox from './StoryBox';
+
+class App extends Component {
+  render() {
+    return (
+      <StoryBox />
+    );
+  }
+}
+
+export default App;
---
+++ src/StoryBox/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class StoryBox extends Component {
+
+  render() {
+    return(<div>Story Box</div>);
+  }
+
+}
+
+export default StoryBox;
